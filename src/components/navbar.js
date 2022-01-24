8a01006d6451643a09819a475193b096855b94c7
Valentino Patane <valentinopatane98@gmail.com>
2022-01-23 21:04:57 -0300

    desafio-3-navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import "./navbar.css"
+
+const Navbar = ()=>{
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Products</li>
+                <li>Gallery</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
